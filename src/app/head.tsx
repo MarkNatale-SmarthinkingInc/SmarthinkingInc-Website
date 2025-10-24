7382a2d452fb6f1de8b9a1a69e8e457cb3cf3f21
Pierre Mouchan <pierre.mouchan@gmail.com>
2025-10-24 17:16:02 +0200

    pagespeed fix

--- src/app/head.tsx
+++ src/app/head.tsx
@@ -3,10 +3,7 @@ import Script from "next/script";
 const Head = () => {
   return (
     <head>
-      <meta
-        name="viewport"
-        content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"
-      />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <Script
         strategy="beforeInteractive"
         // biome-ignore lint/security/noDangerouslySetInnerHtml: <explanation>
