6e81b7ea1db73520f65968a461bca05a4cf96d72
Andrew Gormley <agormley@hey.com>
2026-05-18 10:02:31 -0400

    Updated layout.tsx
    Adding Google Tag Manager code

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,7 +28,29 @@ export default async function RootLayout({
   return (
     <html lang="en">
       <Head />
+      {/* Google Tag Manager - head snippet */}
+      <script
+        dangerouslySetInnerHTML={{
+          __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-NGMSM8ND');`,
+        }}
+      />
+      {/* End Google Tag Manager */}
       <body className="loading" data-barba="wrapper">
+        {/* Google Tag Manager - noscript fallback */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-NGMSM8ND"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
+
         <PageOverlay />
         <Header />
         <Navigation />
