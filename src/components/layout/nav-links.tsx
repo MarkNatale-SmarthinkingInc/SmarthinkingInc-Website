06c20f66330d0423c3601caa1205585ebf27d1ba
Andrew Gormley <agormley@hey.com>
2026-05-21 11:06:24 -0400

    Fix build error with new nav-links.tsx file

--- src/components/layout/nav-links.tsx
+++ src/components/layout/nav-links.tsx
@@ -3,33 +3,33 @@
 import { usePathname } from "next/navigation";
 
 type NavLink = {
-  href: string;
-  text: string | null | undefined;
+	href: string;
+	text: string | null | undefined;
 };
 
 export default function NavLinks({ links }: { links: NavLink[] }) {
-  const pathname = usePathname();
+	const pathname = usePathname();
 
-  return (
-	<div className="nav-links">
-	  {links.map((link) => {
-		const linkPathname = new URL(link.href, window.location.origin).pathname;
-		const isActive = linkPathname === pathname;
+	return (
+		<div className="nav-links">
+			{links.map((link) => {
+				const linkPathname = new URL(link.href).pathname;
+				const isActive = linkPathname === pathname;
 
-		if (isActive) {
-		  return (
-			<span key={link.text} aria-current="page">
-			  {link.text}
-			</span>
-		  );
-		}
+				if (isActive) {
+					return (
+						<span key={link.text} aria-current="page">
+							{link.text}
+						</span>
+					);
+				}
 
-		return (
-		  <a key={link.text} href={link.href}>
-			{link.text}
-		  </a>
-		);
-	  })}
-	</div>
-  );
+				return (
+					<a key={link.text} href={link.href}>
+						{link.text}
+					</a>
+				);
+			})}
+		</div>
+	);
 }
