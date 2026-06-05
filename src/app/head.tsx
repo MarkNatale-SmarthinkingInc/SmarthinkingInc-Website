53cf7f1c0fa5fa3b36d91e6704b6b980586d55d1
Andrew Gormley <agormley@hey.com>
2026-06-05 12:16:54 -0400

    GTM Fix
    Moved GTM to header per installation instructions

--- src/app/head.tsx
+++ src/app/head.tsx
@@ -11,6 +11,18 @@ const Head = () => {
           __html: `history.scrollRestoration = "manual"`,
         }}
       />
+      {/* Google Tag Manager */}
+      <script
+        // biome-ignore lint/security/noDangerouslySetInnerHtml: <explanation>
+        dangerouslySetInnerHTML={{
+          __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-NGMSM8ND');`,
+        }}
+      />
+      {/* End Google Tag Manager */}
     </head>
   );
 };
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,17 +28,6 @@ export default async function RootLayout({
   return (
     <html lang="en">
       <Head />
-      {/* Google Tag Manager - head snippet */}
-      <script
-        dangerouslySetInnerHTML={{
-          __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-NGMSM8ND');`,
-        }}
-      />
-      {/* End Google Tag Manager */}
       <body className="loading" data-barba="wrapper">
         {/* Google Tag Manager - noscript fallback */}
         <noscript>
