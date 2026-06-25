744eb21481c065ab6f393eec23accf995ee80862
Andrew Gormley <agormley@hey.com>
2026-06-25 12:16:58 -0400

    Updated Contact Form
    Hidden field changes

--- src/components/contact/contact-detail-section.tsx
+++ src/components/contact/contact-detail-section.tsx
@@ -98,8 +98,8 @@ export default function ContactDetailSection({ data }: ContactDetailProps) {
           </div>
         ) : (
           <form onSubmit={handleSubmit} className="fadeUp">
-            <input type="hidden" name="acton_form_id" value="fd7409c0-79ec-4d10-8e8e-a7d327b7cfaa" />
-            <input type="hidden" name="form_name" value="Email Signup" />
+            <input type="hidden" name="acton_form_id" value="505767b6-e8f0-4573-9dec-af7437642c50" />
+            <input type="hidden" name="form_name" value="Contact" />
             {state.error && (
               <div
                 className="error-message"
