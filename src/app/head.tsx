ae6ea75fde8ccc4dec6589e3da94c4be7c1d39ca
Andrew Gormley <agormley@hey.com>
2026-07-29 13:42:27 -0400

    Added dev runtime + fixed duplicate viewport tag
    Next automatically generates and inserts one, so I removed mine

--- src/app/head.tsx
+++ src/app/head.tsx
@@ -3,7 +3,6 @@ import Script from "next/script";
 const Head = () => {
 	return (
 		<head>
-			<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 			<Script
 				strategy="beforeInteractive"
 				// biome-ignore lint/security/noDangerouslySetInnerHtml: <explanation>
